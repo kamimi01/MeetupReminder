2ee95f20ad7d4b0fac3a88c1cf0cd7e8d5e93ca9
mika.urakawa <awakaru0122@gmail.com>
2023-01-24 06:45:16 +0900

    add: first commit

---
+++ MeetupReminder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MeetupReminder
+//
+//  Created by mikaurakawa on 2023/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeetupReminder/MeetupReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetupReminderApp.swift
+//  MeetupReminder
+//
+//  Created by mikaurakawa on 2023/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MeetupReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
