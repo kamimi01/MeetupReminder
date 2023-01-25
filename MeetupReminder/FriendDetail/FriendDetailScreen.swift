643eeae34fada6d24f55c0cb4dfc8b20ab7d0829
mika.urakawa <awakaru0122@gmail.com>
2023-01-25 21:09:38 +0900

    add: friend detail view

--- MeetupReminder/FriendDetail/FriendDetailScreen.swift
+++ MeetupReminder/FriendDetail/FriendDetailScreen.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct FriendDetailScreen: View {
+    let cardColor: CardViewColor
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.mainBackground
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                ZStack(alignment: .center) {
+                    Rectangle()
+                        .foregroundColor(cardColor.carViewBackground)
+                        .frame(height: UIScreen.main.bounds.height / 2.5)
+                    profileImage
+                }
+                .edgesIgnoringSafeArea(.all)
+                Spacer()
+            }
+        }
+    }
+}
+
+private extension FriendDetailScreen {
+    var profileImage: some View {
+        Image(cardColor.profileImage)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .background(Color.mainBackground)
+            .clipShape(Circle())
+            .frame(maxWidth: 130, maxHeight: 130)
     }
 }
 
 struct FriendDetailScreen_Previews: PreviewProvider {
     static var previews: some View {
-        FriendDetailScreen()
+        FriendDetailScreen(cardColor: .red)
     }
 }
